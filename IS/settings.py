198a9d60c6a6adeb3a2c6bdb2bb957c24643f5b0
gowthamgowda4123 <gowthamgowda4123@gmail.com>
2020-08-27 10:00:36 +0530

    Update settings.py

--- IS/settings.py
+++ IS/settings.py
@@ -134,9 +134,9 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 ###########################################################################
-AWS_ACCESS_KEY_ID = 'AKIAWRBBLOFCPOEAC3Q6'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 's3examplestatic'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = ''
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
